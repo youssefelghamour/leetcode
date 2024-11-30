2fe9d66e77286dbd75e50beab23acf3a412146fe
youssefelghamour <youssefelghamour@gmail.com>
2024-11-30 15:38:13 +0100

    55. Jump Game

---
+++ JavaScript/Jump Game/canJump.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    target = nums.length - 1
+    
+    for (let i = nums.length - 2; i >= 0; i--) {
+        if ((i + nums[i]) >= target) {
+            target = i
+        }
+    }
+    
+    return target === 0 ? true : false
+};
