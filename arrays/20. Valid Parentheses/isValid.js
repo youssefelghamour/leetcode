07a33ab170896b749c1208ccbf6a49f50d4005e5
youssefelghamour <youssefelghamour@gmail.com>
2025-01-12 16:54:55 +0100

    20. Valid Parentheses

--- arrays/20. Valid Parentheses/isValid.js
+++ arrays/20. Valid Parentheses/isValid.js
@@ -4,22 +4,22 @@
  */
 var isValid = function(s) {
     const matchingBrackets = {
-          ')': '(',
-          '}': '{',
-          ']': '['
-      };
-  
-      const stack = [];
-  
-      for (let c of s) {
-          if (Object.values(matchingBrackets).includes(c)) {  // Opening brackets
-              stack.push(c);
-          } else if (c in matchingBrackets) {  // Closing brackets
-              if (stack.length === 0 || stack.pop() !== matchingBrackets[c]) {
-                  return false;
-              }
-          }
-      }
-  
-      return stack.length === 0;  
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    const stack = [];
+
+    for (let c of s) {
+        if (Object.values(matchingBrackets).includes(c)) {  // Opening brackets
+            stack.push(c);
+        } else if (c in matchingBrackets) {  // Closing brackets
+            if (stack.length === 0 || stack.pop() !== matchingBrackets[c]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;  
   };
