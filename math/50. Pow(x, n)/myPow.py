f03a7fed046d177c152b1c66ad9e09217b8b2355
youssefelghamour <youssefelghamour@gmail.com>
2025-01-27 21:35:51 +0100

    50. Pow(x, n)

--- math/50. Pow(x, n)/myPow.py
+++ math/50. Pow(x, n)/myPow.py
@@ -1,6 +1,6 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
+        """
         def pow(x, n):
             r = 1
             for i in range(1, n+1):
@@ -16,3 +16,23 @@ class Solution:
                 return pow(1/x, abs(n))
         if n == 0:
             return 1
+        """
+        def pow(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            res = pow(x, n // 2)
+            res *= res
+            
+            if n % 2 != 0:
+                return x * res
+            else:
+                return res
+            
+        
+        if n >= 0:
+            return pow(x, n)
+        else:
+            return 1 / pow(x, abs(n))
