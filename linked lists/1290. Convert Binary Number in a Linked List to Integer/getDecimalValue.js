64b3d9603dc1245e22eb9be219d0f368606af026
youssef <youssefelghamour@gmail.com>
2025-04-05 10:45:18 +0000

    1290. Convert Binary Number in a Linked List to Integer

---
+++ linked lists/1290. Convert Binary Number in a Linked List to Integer/getDecimalValue.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let number = 0;
+
+    while (head) {
+        number = (number * 2) + head.val;
+        head = head.next;
+    }
+
+    return number;
+};
