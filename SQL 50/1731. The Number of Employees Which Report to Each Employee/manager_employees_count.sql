37eb8a294d2f4e385b09aded2b509bcc9c199574
youssef <youssefelghamour@gmail.com>
2025-04-15 12:48:46 +0100

    1731. The Number of Employees Which Report to Each Employee

---
+++ SQL 50/1731. The Number of Employees Which Report to Each Employee/manager_employees_count.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    e2.employee_id,
+    e2.name,
+    COUNT(e1.employee_id) AS reports_count,
+    ROUND(AVG(e1.age)) AS average_age
+FROM Employees e1
+JOIN Employees e2
+    ON e1.reports_to = e2.employee_id
+WHERE e1.reports_to IS NOT NULL
+GROUP BY e1.reports_to
+ORDER BY e2.employee_id;
