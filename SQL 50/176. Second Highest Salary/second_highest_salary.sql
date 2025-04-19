11265a584fd326cee3162b0b2fe5ac82a8863bd0
youssef <youssefelghamour@gmail.com>
2025-04-19 20:13:49 +0100

    176. Second Highest Salary

---
+++ SQL 50/176. Second Highest Salary/second_highest_salary.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (
+    SELECT MAX(salary) AS HighestSalary
+    FROM Employee
+);
