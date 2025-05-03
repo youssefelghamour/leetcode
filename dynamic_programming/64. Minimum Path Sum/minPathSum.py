ae7532834a776a4d141b26c778a84eb9dc4ba244
youssef <youssefelghamour@gmail.com>
2025-05-03 18:29:54 +0100

    64. Minimum Path Sum

--- dynamic_programming/64. Minimum Path Sum/minPathSum.py
+++ dynamic_programming/64. Minimum Path Sum/minPathSum.py
@@ -33,6 +33,7 @@ class Solution(object):
         # Return the value of the bottom right cell that contains now the min path sum leading to it
         return sum_matrix[-1][-1]
         """
+        """
         # Updating the matric in place
         for r in range(len(grid)):
             for c in range(len(grid[0])):
@@ -50,4 +51,20 @@ class Solution(object):
                         # Add the min path sum between the top and left
                         grid[r][c] += min(grid[r-1][c], grid[r][c-1])
         
+        return grid[-1][-1]
+        """
+        m, n = len(grid), len(grid[0])
+
+        # First row
+        for c in range(1,n):
+            grid[0][c] += grid[0][c-1]
+
+        # First column
+        for r in range(1,m):
+            grid[r][0] += grid[r-1][0]
+
+        for r in range(1,m):
+            for c in range(1,n):
+                grid[r][c] += min(grid[r-1][c], grid[r][c-1])
+
         return grid[-1][-1]
