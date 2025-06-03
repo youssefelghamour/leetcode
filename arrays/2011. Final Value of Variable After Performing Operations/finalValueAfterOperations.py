5413def4d0e0f35be3aefcaba86a9e0a58068b38
youssef <youssefelghamour@gmail.com>
2025-06-03 20:06:10 +0100

    2011. Final Value of Variable After Performing Operations

---
+++ arrays/2011. Final Value of Variable After Performing Operations/finalValueAfterOperations.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        x = 0
+        for op in operations:
+            if "+" in op:
+                x += 1
+            else:
+                x -= 1
+        return x
