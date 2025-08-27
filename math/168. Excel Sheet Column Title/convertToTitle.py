c1300e0af44fe8ec6a05355491cb67edb0a08d82
youssef <youssefelghamour@gmail.com>
2025-08-27 20:52:23 +0100

    168. Excel Sheet Column Title

---
+++ math/168. Excel Sheet Column Title/convertToTitle.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+
+        ex: columnNumber = 701 = 26*26 + 25
+
+            701 % 26 = 25 → letter 'Y'
+            701 - 25 = 676 → 676 // 26 = 26
+
+            26 % 26 = 0 → use 26 → letter 'Z'
+            26 - 26 = 0 → 0 // 26 = 0 → stop
+        """
+        columns = {
+            1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J',
+            11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S',
+            20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z'
+        }
+        result = ""
+
+        while columnNumber > 0:
+            mod = columnNumber % 26
+
+            # in case columnNumber = 26 -> 26 % 26 = 0 breaks code -> choose 26 map to 'Z'
+            mod = 26 if mod == 0 else mod
+            char = columns[mod]
+
+            columnNumber -= mod
+            columnNumber //= 26
+
+            result = char + result
+        
+        return result
