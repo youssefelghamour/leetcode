c32d0eb73e8dd8ceb6c571cb5a5be6252b419c24
youssef <youssefelghamour@gmail.com>
2025-10-10 16:17:26 +0100

    334. Increasing Triplet Subsequence

---
+++ leetcode 75/334. Increasing Triplet Subsequence/increasingTriplet.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let a = Infinity;
+    let b = Infinity;
+
+    for (let num of nums) {
+        if (num <= a) a = num;
+        else if (num <= b) b= num;
+        else return true;
+    }
+
+    return false;
+};
